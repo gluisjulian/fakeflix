d2357e8e990fdc9ef52df418627dc37e69091bd5
gluisjulian <gluisjulian@gmail.com>
2026-09-18 21:32:25 -0300

    detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,8 +1,67 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
+
+import './filme-info.css';
+
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "577d2f5b971dd09c01fbd6510959c091",
+                    language:"pt-BR",
+                }
+            })
+            .then((response) => {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log("Filme não encontrado.")
+            })
+        }
+
+        loadFilme();
+
+        return() => {
+            console.log("Componente desmontado");
+        }
+    }, [])
+
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h1>Carregando detalhes do filme...</h1>
+            </div>
+        )
+    }
+
+
     return(
 
-        <div>
-            <h1>Bem vindo a pagina detalhes do filme</h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+        <div className='area-buttons'>
+            <button>Salvar</button>
+            <button>
+                <a href='#'>
+                    Trailer
+                </a>
+            </button>
+        </div>
+
         </div>
     )
 }
