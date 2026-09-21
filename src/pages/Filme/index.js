1ebb2f756ef245eebb6397247be7d632e9e65c61
gluisjulian <gluisjulian@gmail.com>
2026-09-21 13:58:55 -0300

    Funcionamento do botao trailer

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import api from '../../services/api';
 
 import './filme-info.css';
 
 function Filme(){
     const { id } = useParams();
+    const navigate = useNavigate();
+
+
     const [filme, setFilme] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -22,7 +25,8 @@ function Filme(){
                 setLoading(false);
             })
             .catch(() => {
-                console.log("Filme não encontrado.")
+                navigate("/", { replace:true })
+                return;
             })
         }
 
@@ -31,7 +35,7 @@ function Filme(){
         return() => {
             console.log("Componente desmontado");
         }
-    }, [])
+    }, [navigate, id])
 
 
     if(loading){
@@ -56,7 +60,7 @@ function Filme(){
         <div className='area-buttons'>
             <button>Salvar</button>
             <button>
-                <a href='#'>
+                <a target='_blank' rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer` }>
                     Trailer
                 </a>
             </button>
